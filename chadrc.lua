6804186e90595f4fb46d1ecf9fedc3e1217e7380
asrul10 <hanafi.asrul@gmail.com>
2022-11-04 13:38:17 +0700

    set relative number

--- chadrc.lua
+++ chadrc.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.ui = {
   theme_toggle = { "onedark", "one_light" },
-  theme = "onedark",
+  theme = "monekai",
 }
 
 M.plugins = require "custom.plugins"
--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 local autocmd = vim.api.nvim_create_autocmd
+local opt = vim.opt
 
+opt.number = true
+opt.numberwidth = 1
+opt.relativenumber = true
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
