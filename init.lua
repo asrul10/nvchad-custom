92e120a98c0e75211ae98f4e8fbfbeb0ed8c95a1
Asrul Hanafi <asrul@pop-os.localdomain>
2022-11-21 06:55:00 +0700

    feat: highlight yanked

--- init.lua
+++ init.lua
@@ -4,6 +4,15 @@ local opt = vim.opt
 opt.number = true
 opt.numberwidth = 1
 opt.relativenumber = true
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = "Hightlight selection on yank",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank { higroup = "IncSearch", timeout = 500 }
+  end,
+})
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
