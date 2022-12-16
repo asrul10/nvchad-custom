a3981879b73629b6bff42a1220f722bd744eb7cc
asrul10 <asrul@kotaxdev.com>
2022-12-17 06:25:12 +0700

    feat: find and replace shortcut

--- mappings.lua
+++ mappings.lua
@@ -1,6 +1,10 @@
 local M = {}
 
 M.general = {
+  n = {
+    ["<leader>sa"] = { ":%s/", "Find and replace all" },
+    ["<leader>ss"] = { ":.,.+", "Find and replace current cursor to ++" },
+  },
   v = {
     ["J"] = { ":m '>+1<CR>gv=gv", "move line down" },
     ["K"] = { ":m '<-2<CR>gv=gv", "move line up" },
