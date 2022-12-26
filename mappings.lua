968cd22428dfa3628c734cc1a97d5042f6c2d4c1
asrul10 <asrul@kotaxdev.com>
2022-12-26 12:29:40 +0700

    feat: line break by comma

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,7 @@ M.general = {
   n = {
     ["<leader>sa"] = { ":%s/", "Find and replace all" },
     ["<leader>ss"] = { ":.,.+", "Find and replace current cursor to ++" },
+    ["<leader>sj"] = { ":s/, /,\\r/g<CR>", "Comma and line break" },
   },
   v = {
     ["J"] = { ":m '>+1<CR>gv=gv", "move line down" },
